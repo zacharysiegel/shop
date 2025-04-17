2b08bee9ff48a064ecf9663ea9a9ced46f4123bc
zacharysiegel <siegelzc@gmail.com>
2025-04-16 21:51:24 -0500

    middleware logger at warn level

--- storefront/src/server.rs
+++ storefront/src/server.rs
@@ -1,13 +1,14 @@
 mod route;
 mod state;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{App, HttpServer, web};
 use state::AppState;
 
 pub async fn open_server() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .app_data(web::Data::new(AppState::new()))
+            .wrap(actix_web::middleware::Logger::default())
             .configure(route::configuration)
     })
     .bind("127.0.0.1:8080")?
