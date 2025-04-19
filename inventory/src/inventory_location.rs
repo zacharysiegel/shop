a2fc901fd4aa2ba8dec1fe8d0281af397efb1b50
zacharysiegel <siegelzc@gmail.com>
2025-04-19 14:58:42 -0500

    get_all_inventory_locations

--- inventory/src/inventory_location.rs
+++ inventory/src/inventory_location.rs
@@ -1,4 +1,5 @@
 use crate::InventoryResource;
+use crate::server::JsonHttpResponse;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
@@ -37,10 +38,13 @@ pub struct InventoryLocationSerial {
 	pub internal_name: String,
 }
 
+impl JsonHttpResponse for InventoryLocationSerial {}
+impl JsonHttpResponse for Vec<InventoryLocationSerial> {}
+
 pub mod db {
 	use crate::inventory_location::InventoryLocation;
 	use sqlx::postgres::PgQueryResult;
-	use sqlx::{Error, PgPool, query};
+	use sqlx::{Error, PgPool, query, query_as};
 
 	pub async fn create_inventory_location(
 		pgpool: &PgPool,
@@ -58,16 +62,31 @@ pub mod db {
 		.execute(pgpool)
 		.await
 	}
+
+	pub async fn get_all_inventory_locations(
+		pgpool: &PgPool,
+	) -> Result<Vec<InventoryLocation>, Error> {
+		query_as!(
+			InventoryLocation,
+			"select id, display_name, internal_name from inventory_location"
+		)
+		.fetch_all(pgpool)
+		.await
+	}
 }
 
 pub mod route {
 	use super::*;
 	use actix_web::http::StatusCode;
-	use actix_web::{HttpResponseBuilder, Responder, post, web};
+	use actix_web::{HttpResponseBuilder, Responder, get, post, web};
 	use sqlx::{Pool, Postgres};
 
 	pub fn configurer(config: &mut web::ServiceConfig) {
-		config.service(web::scope("/inventory_location").service(create_inventory_location));
+		config.service(
+			web::scope("/inventory_location")
+				.service(create_inventory_location)
+				.service(get_all_inventory_locations),
+		);
 	}
 
 	#[post("")]
@@ -78,11 +97,29 @@ pub mod route {
 		let inventory_location = body.into_inner();
 		let inventory_location = InventoryLocation::from_serial(&inventory_location);
 
-		let result = super::db::create_inventory_location(&pgpool, inventory_location).await;
+		let result = db::create_inventory_location(&pgpool, inventory_location).await;
 		match result {
 			Ok(query_result) => HttpResponseBuilder::new(StatusCode::CREATED)
 				.body(query_result.rows_affected().to_string()),
 			Err(_) => HttpResponseBuilder::new(StatusCode::INTERNAL_SERVER_ERROR).finish(),
 		}
+	}
+
+	#[get("")]
+	async fn get_all_inventory_locations(pgpool: web::Data<Pool<Postgres>>) -> impl Responder {
+		let result = db::get_all_inventory_locations(&pgpool).await;
+		let inventory_locations = match result {
+			Ok(inventory_locations) => inventory_locations,
+			Err(e) => {
+				log::error!("Error fetching inventory locations; [{:#}];", e);
+				return HttpResponseBuilder::new(StatusCode::INTERNAL_SERVER_ERROR).finish();
+			}
+		};
+
+		inventory_locations
+			.iter()
+			.map(|inventory_location| InventoryLocation::to_serial(inventory_location))
+			.collect::<Vec<InventoryLocationSerial>>()
+			.to_http_response()
 	}
 }
