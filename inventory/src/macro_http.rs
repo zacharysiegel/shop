45a129849f9625960e59469712ef65663cabda68
zacharysiegel <siegelzc@gmail.com>
2025-04-24 00:37:42 -0500

    std hygene in macro_http

--- inventory/src/macro_http.rs
+++ inventory/src/macro_http.rs
@@ -2,8 +2,8 @@
 macro_rules! unwrap_result_else_400 {
     ($e:expr) => {
         match $e {
-            Ok(content) => content,
-            Err(error) => {
+            ::std::result::Result::Ok(content) => content,
+            ::std::result::Result::Err(error) => {
                 ::log::error!("Error unwrapping Result; Returning 400 Bad Request; [{:#}];", error.to_string());
                 return ::actix_web::HttpResponse::BadRequest().finish();
             },
@@ -15,8 +15,8 @@ macro_rules! unwrap_result_else_400 {
 macro_rules! unwrap_result_else_500 {
     ($e:expr) => {
         match $e {
-            Ok(content) => content,
-            Err(error) => {
+            ::std::result::Result::Ok(content) => content,
+            ::std::result::Result::Err(error) => {
                 ::log::error!("Error unwrapping Result; Returning 500 Internal Server Error; [{:#}];", error.to_string());
                 return ::actix_web::HttpResponse::InternalServerError().finish();
             },
@@ -28,8 +28,8 @@ macro_rules! unwrap_result_else_500 {
 macro_rules! unwrap_option_else_404 {
     ($e:expr) => {
         match $e {
-            Some(content) => content,
-            None => {
+            ::std::option::Option::Some(content) => content,
+            ::std::option::Option::None => {
                 ::log::error!("Error unwrapping Option; Returning 404 Not Found;");
                 return ::actix_web::HttpResponse::NotFound().finish();
             },
