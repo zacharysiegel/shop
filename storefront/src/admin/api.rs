0d63314a77a084de6551b381f2ef879fb305bfba
zacharysiegel <siegelzc@gmail.com>
2025-05-10 18:01:30 -0500

    use internal url rather than forwarding cookie for auth

--- storefront/src/admin/api.rs
+++ storefront/src/admin/api.rs
@@ -1,43 +1,11 @@
 use crate::admin::structure::error_text::error_markup;
 use crate::registry::REGISTRY;
 use maud::Markup;
-use reqwest::RequestBuilder;
 use serde::de::DeserializeOwned;
 
 pub async fn wrapped_get<SerialT: DeserializeOwned>(path_and_query: &str) -> Result<SerialT, Markup> {
     let result = REGISTRY.http_client
         .get(format!("{}{}", REGISTRY.remote_url, path_and_query))
-        .send()
-        .await;
-    let response = match result {
-        Ok(response) => response,
-        Err(error) => {
-            return Err(error_markup(error));
-        }
-    };
-    let serial = match response.json::<SerialT>().await {
-        Ok(element) => element,
-        Err(error) => {
-            return Err(error_markup(error));
-        }
-    };
-    Ok(serial)
-}
-
-pub async fn wrapped_get_cookie<SerialT: DeserializeOwned>(
-    path_and_query: &str,
-    cookie: Option<&actix_web::http::header::HeaderValue>
-) -> Result<SerialT, Markup> {
-    log::info!("{}", cookie.unwrap().to_str().unwrap());
-    let request_builder: RequestBuilder = REGISTRY.http_client
-        .get(format!("{}{}", REGISTRY.remote_url, path_and_query));
-
-    let request_builder = match cookie {
-        Some(cookie) => request_builder.header("Cookie", cookie.to_str().unwrap()),
-        None => request_builder,
-    };
-
-    let result = request_builder
         .send()
         .await;
     let response = match result {
--- storefront/src/registry.rs
+++ storefront/src/registry.rs
@@ -5,13 +5,14 @@ use std::sync::LazyLock;
 
 pub struct Registry {
     pub http_client: Client,
+    /// To access the inventory application, we use the internal URI rather than the public one which requires authentication.
     pub remote_url: String,
 }
 
 pub static REGISTRY: LazyLock<Registry> = LazyLock::new(|| {
     Registry {
         http_client: create_http_client(),
-        remote_url: env::var("REMOTE_URL").unwrap_or("https://127.0.0.1:1443/api".to_string()),
+        remote_url: env::var("REMOTE_URL").unwrap_or("http://127.0.0.1:11001".to_string()),
     }
 });
 
