0cc694b94165c89af312d87d05213f3fdb224d4c
zacharysiegel <siegelzc@gmail.com>
2025-05-13 22:12:30 -0500

    rename

--- crypt/src/data.rs
+++ crypt/src/data.rs
@@ -5,7 +5,7 @@ use std::sync::LazyLock;
 pub const SECRETS: LazyLock<BTreeMap<&'static str, SecretBase64>> = LazyLock::new(|| {
     let mut map: BTreeMap<&'static str, SecretBase64> = BTreeMap::new();
     map.insert(
-        "ebay_cert_id_zach",
+        "ebay__zach.sandbox.cert_id",
         SecretBase64 {
             nonce: String::from("vzB3tLkRz0xnv4Ud"),
             ciphertext: String::from("BZjNxWZfKjDhbsuB87qPtW1pDGgUd7yI6iHXGDK04TYgTKfLDlbgaGyFKOVlnaBF7YRt7Q=="),
