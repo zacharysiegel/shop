77cc0a2174cf73a0e42c3185468cabdff7a3c318
zacharysiegel <siegelzc@gmail.com>
2025-05-15 18:30:26 -0500

    client_id

--- inventory/src/registry.rs
+++ inventory/src/registry.rs
@@ -5,6 +5,7 @@ use std::sync::LazyLock;
 pub struct Registry {
     pub http_client: Client,
     pub ebay_base_url: &'static str,
+    pub ebay_client_id: &'static str,
 }
 
 pub static REGISTRY: LazyLock<Registry> = LazyLock::new(|| {
@@ -16,5 +17,7 @@ pub static REGISTRY: LazyLock<Registry> = LazyLock::new(|| {
             RuntimeEnvironment::Local | RuntimeEnvironment::Stage => "https://api.sandbox.ebay.com/",
             RuntimeEnvironment::Production => "https://api.ebay.com/"
         },
+        // This value pertains to my testing "zach" account. Presumably this will change to an official eBay account.
+        ebay_client_id: "ZacharyS-shop-SBX-9a6e149a0-59597965",
     }
 });
