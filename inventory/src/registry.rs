f83a1cc623083e22ef3a63e9155f7b4ff07c6013
zacharysiegel <siegelzc@gmail.com>
2025-05-14 22:48:34 -0500

    ebay_base_url

--- inventory/src/registry.rs
+++ inventory/src/registry.rs
@@ -1,12 +1,20 @@
+use crate::environment::RuntimeEnvironment;
 use reqwest::Client;
 use std::sync::LazyLock;
 
 pub struct Registry {
     pub http_client: Client,
+    pub ebay_base_url: &'static str,
 }
 
 pub static REGISTRY: LazyLock<Registry> = LazyLock::new(|| {
+    let environment = RuntimeEnvironment::default();
+
     Registry {
         http_client: Client::builder().build().unwrap(),
+        ebay_base_url: match environment {
+            RuntimeEnvironment::Local | RuntimeEnvironment::Stage => "https://api.sandbox.ebay.com/",
+            RuntimeEnvironment::Production => "https://api.ebay.com/"
+        },
     }
 });
