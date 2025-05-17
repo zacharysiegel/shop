f5b360673e028af68b9698ab002e667b52fe6520
zacharysiegel <siegelzc@gmail.com>
2025-05-16 20:42:23 -0500

    get_user_token

--- inventory/src/marketplace/ebay/ebay_api.rs
+++ inventory/src/marketplace/ebay/ebay_api.rs
@@ -1,20 +1,44 @@
-use crate::unwrap_result_else_500;
+use crate::environment::RuntimeEnvironment;
+use crate::{unwrap_result_else_400, unwrap_result_else_500};
 use actix_web::web::ServiceConfig;
 use actix_web::{web, HttpResponse, Responder};
+use std::collections::BTreeMap;
+use std::sync::LazyLock;
+use crate::marketplace::ebay::ebay_client::{AuthorizationCodeResponse, ClientCredentialsResponse};
+
+static EBAY_OAUTH_AUTHORIZATION_URL: LazyLock<&str> = LazyLock::new(|| match RuntimeEnvironment::default() {
+    RuntimeEnvironment::Local | RuntimeEnvironment::Stage => "https://auth.sandbox.ebay.com/oauth2/authorize?client_id=ZacharyS-shop-SBX-9a6e149a0-59597965&response_type=code&redirect_uri=Zachary_Siegel-ZacharyS-shop-S-kdujedb&scope=https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/buy.order.readonly https://api.ebay.com/oauth/api_scope/buy.guest.order https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly https://api.ebay.com/oauth/api_scope/sell.marketplace.insights.readonly https://api.ebay.com/oauth/api_scope/commerce.catalog.readonly https://api.ebay.com/oauth/api_scope/buy.shopping.cart https://api.ebay.com/oauth/api_scope/buy.offer.auction https://api.ebay.com/oauth/api_scope/commerce.identity.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.email.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.phone.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.address.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.name.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.status.readonly https://api.ebay.com/oauth/api_scope/sell.finances https://api.ebay.com/oauth/api_scope/sell.payment.dispute https://api.ebay.com/oauth/api_scope/sell.item.draft https://api.ebay.com/oauth/api_scope/sell.item https://api.ebay.com/oauth/api_scope/sell.reputation https://api.ebay.com/oauth/api_scope/sell.reputation.readonly https://api.ebay.com/oauth/api_scope/commerce.notification.subscription https://api.ebay.com/oauth/api_scope/commerce.notification.subscription.readonly https://api.ebay.com/oauth/api_scope/sell.stores https://api.ebay.com/oauth/api_scope/sell.stores.readonly",
+    RuntimeEnvironment::Production => "todo", // todo: update this and the testing url when we have an official eBay account.
+});
 
 pub fn configurer(config: &mut ServiceConfig) {
     config.service(
         web::scope("/ebay")
-            .route("/auth/application_token", web::get().to(get_application_token))
-            .route("/auth/user_token", web::get().to(get_user_token))
+            .route("/auth/application/token", web::get().to(get_application_token))
+            .route("/auth/user/token", web::get().to(get_user_token))
+            .route("/auth/user/redirect", web::get().to(get_oauth_redirect))
     );
 }
 
 async fn get_application_token() -> impl Responder {
-    let token: String = unwrap_result_else_500!(super::ebay_client::get_application_token().await);
-    HttpResponse::Ok().body(token)
+    let token_response: ClientCredentialsResponse = unwrap_result_else_500!(
+        super::ebay_client::get_application_token().await
+    );
+    HttpResponse::Ok().json(token_response)
 }
 
-async fn get_user_token() -> impl Responder {
-    HttpResponse::Gone().finish() // todo
+async fn get_user_token(
+    query: web::Query<BTreeMap<String, String>>,
+) -> impl Responder {
+    let authorization_code: &String = unwrap_result_else_400!(query.get("code").ok_or("Missing authorization code"));
+    let user_token_response: AuthorizationCodeResponse = unwrap_result_else_500!(
+        super::ebay_client::get_user_token(authorization_code).await
+    );
+    HttpResponse::Ok().json(user_token_response)
+}
+
+async fn get_oauth_redirect() -> impl Responder {
+    HttpResponse::Found()
+        .insert_header(("Location", EBAY_OAUTH_AUTHORIZATION_URL.to_string()))
+        .finish()
 }
--- inventory/src/marketplace/ebay/ebay_client.rs
+++ inventory/src/marketplace/ebay/ebay_client.rs
@@ -8,8 +8,8 @@ use crate::listing::Listing;
 use crate::product::Product;
 use base64::Engine;
 use reqwest::header::{AUTHORIZATION, CONTENT_TYPE};
-use reqwest::{Request, Response, StatusCode};
-use serde::Deserialize;
+use reqwest::{Request, Response};
+use serde::{Deserialize, Serialize};
 use std::sync::LazyLock;
 
 static EBAY_BASE_URL: LazyLock<&str> = LazyLock::new(|| match RuntimeEnvironment::default() {
@@ -20,12 +20,9 @@ static EBAY_BASE_URL: LazyLock<&str> = LazyLock::new(|| match RuntimeEnvironment
 // This value pertains to my testing "zach" account. Presumably this will change to an official eBay account.
 /// Presented as a UTF-8-encoded string because this secret must be re-encoded with the client ID in base64 to form the basic authentication credential
 static EBAY_CLIENT_SECRET: LazyLock<String> = LazyLock::new(|| String::from_utf8(master_decrypt("ebay__zach.sandbox.cert_id").unwrap()).unwrap());
-static EBAY_OAUTH_AUTHORIZATION_URL: LazyLock<&str> = LazyLock::new(|| match RuntimeEnvironment::default() {
-    RuntimeEnvironment::Local | RuntimeEnvironment::Stage => "https://auth.sandbox.ebay.com/oauth2/authorize?client_id=ZacharyS-shop-SBX-9a6e149a0-59597965&response_type=code&redirect_uri=Zachary_Siegel-ZacharyS-shop-S-kdujedb&scope=https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/buy.order.readonly https://api.ebay.com/oauth/api_scope/buy.guest.order https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly https://api.ebay.com/oauth/api_scope/sell.marketplace.insights.readonly https://api.ebay.com/oauth/api_scope/commerce.catalog.readonly https://api.ebay.com/oauth/api_scope/buy.shopping.cart https://api.ebay.com/oauth/api_scope/buy.offer.auction https://api.ebay.com/oauth/api_scope/commerce.identity.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.email.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.phone.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.address.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.name.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.status.readonly https://api.ebay.com/oauth/api_scope/sell.finances https://api.ebay.com/oauth/api_scope/sell.payment.dispute https://api.ebay.com/oauth/api_scope/sell.item.draft https://api.ebay.com/oauth/api_scope/sell.item https://api.ebay.com/oauth/api_scope/sell.reputation https://api.ebay.com/oauth/api_scope/sell.reputation.readonly https://api.ebay.com/oauth/api_scope/commerce.notification.subscription https://api.ebay.com/oauth/api_scope/commerce.notification.subscription.readonly https://api.ebay.com/oauth/api_scope/sell.stores https://api.ebay.com/oauth/api_scope/sell.stores.readonly",
-    RuntimeEnvironment::Production => "todo", // todo: update this and the testing url when we have an official eBay account.
-});
 
 const EBAY_CLIENT_ID: &str = "ZacharyS-shop-SBX-9a6e149a0-59597965";
+const EBAY_REDIRECT_URL_NAME: &str = "Zachary_Siegel-ZacharyS-shop-S-kdujedb";
 const EBAY_MARKETPLACE_US: &str = "EBAY_US";
 // https://developer.ebay.com/api-docs/sell/account/types/ba:MarketplaceIdEnum
 const EBAY_CATEGORY_STANDARD: &str = "ALL_EXCLUDING_MOTORS_VEHICLES";
@@ -42,14 +39,14 @@ fn basic_auth() -> String {
     BASE64.encode(raw.as_bytes())
 }
 
-#[derive(Deserialize)]
-struct ClientCredentialsResponse {
+#[derive(Serialize, Deserialize)]
+pub(super) struct ClientCredentialsResponse {
     access_token: String,
     expires_in: u64,
     token_type: String,
 }
 
-pub(super) async fn get_application_token() -> Result<String, ShopError> {
+pub(super) async fn get_application_token() -> Result<ClientCredentialsResponse, ShopError> {
     let request: Request = http::HTTP_CLIENT
         .post(format!("{}{}/token", *EBAY_BASE_URL, OAUTH_API_BASE_PATH))
         .header(CONTENT_TYPE, "x-www-form-urlencoded")
@@ -57,11 +54,37 @@ pub(super) async fn get_application_token() -> Result<String, ShopError> {
         .body("grant_type=client_credentials&scope=https://api.ebay.com/oauth/api_scope+https://api.ebay.com/oauth/api_scope/buy.guest.order+https://api.ebay.com/oauth/api_scope/buy.item.feed+https://api.ebay.com/oauth/api_scope/buy.marketing+https://api.ebay.com/oauth/api_scope/buy.product.feed+https://api.ebay.com/oauth/api_scope/buy.marketplace.insights+https://api.ebay.com/oauth/api_scope/buy.proxy.guest.order+https://api.ebay.com/oauth/api_scope/buy.item.bulk+https://api.ebay.com/oauth/api_scope/buy.deal")
         .build()
         .map_err(|error| ShopError::from_error("malformed request", Box::new(error)))?;
-    let response: Response = http::execute_checked(request).await?;
 
-    let client_credentials_response: ClientCredentialsResponse = response.json::<ClientCredentialsResponse>().await
+    let response: Response = http::execute_checked(request).await?;
+    let body: ClientCredentialsResponse = response.json::<ClientCredentialsResponse>().await
         .map_err(|error| ShopError::from_error("parsing response", Box::new(error)))?;
-    Ok(client_credentials_response.access_token)
+
+    Ok(body)
+}
+
+#[derive(Serialize, Deserialize)]
+pub(super) struct AuthorizationCodeResponse {
+    access_token: String,
+    expires_in: u64,
+    refresh_token: String,
+    refresh_token_expires_in: u64,
+    token_type: String,
+}
+
+pub(super) async fn get_user_token(authorization_code: &str) -> Result<AuthorizationCodeResponse, ShopError> {
+    let request: Request = http::HTTP_CLIENT
+        .post(format!("{}{}/token", *EBAY_BASE_URL, OAUTH_API_BASE_PATH))
+        .header(CONTENT_TYPE, "x-www-form-urlencoded")
+        .header(AUTHORIZATION, format!("Basic {}", basic_auth()))
+        .body(format!("grant_type=authorization_code&redirect_uri={}&code={}", EBAY_REDIRECT_URL_NAME, authorization_code))
+        .build()
+        .map_err(|error| ShopError::from_error("malformed request", Box::new(error)))?;
+
+    let response: Response = http::execute_checked(request).await?;
+    let body: AuthorizationCodeResponse = response.json::<AuthorizationCodeResponse>().await
+        .map_err(|error| ShopError::from_error("parsing response", Box::new(error)))?;
+
+    Ok(body)
 }
 
 pub(super) async fn create_listing(
