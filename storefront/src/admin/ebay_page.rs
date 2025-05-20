21a73e4b639a017fe1c898fe23472613487971fa
zacharysiegel <siegelzc@gmail.com>
2025-05-19 20:09:41 -0500

    refresh token via ui

--- storefront/src/admin/ebay_page.rs
+++ storefront/src/admin/ebay_page.rs
@@ -1,12 +1,12 @@
-use std::ops::Deref;
 use crate::admin::structure::breadcrumb::BreadcrumbItem;
 use crate::admin::structure::page::Page;
 use crate::admin::structure::{form, page};
 use actix_web::web;
 use actix_web::web::ServiceConfig;
+use inventory::environment::RuntimeEnvironment;
 use maud::{html, Markup};
 use reqwest::Method;
-use inventory::environment::RuntimeEnvironment;
+use std::ops::Deref;
 
 pub const PAGE: Page = Page {
     name: "eBay",
@@ -30,10 +30,11 @@ async fn render() -> Markup {
 fn content() -> Markup {
     let runtime_environment: RuntimeEnvironment = RuntimeEnvironment::default();
     html! {
-        h2 { "eBay integration" }
+        h1 { "eBay integration" }
 
         @if runtime_environment == RuntimeEnvironment::Local {
             (auth_local())
+            (refresh())
         }
     }
 }
@@ -41,19 +42,30 @@ fn content() -> Markup {
 fn auth_local() -> Markup {
     let oauth_url: &&str = inventory::marketplace::ebay::ebay_api::EBAY_OAUTH_AUTHORIZATION_URL.deref();
     html! {
-        p {
-            a href=(oauth_url) target="_blank" rel="noopener noreferrer" { "Authorization code redirect" }
-        }
-        p {
-            r#"Authenticate with your eBay "TESTUSER_*" account. After logging in, you will be redirected to a blank landing page. The landing page URL contains a "code" query parameter. Copy that code and enter it below."#
-        }
-        br;
-        (form::form("Fetch user tokens", "/ebay/auth/user/token", Method::PUT, html! {
+        (form::form("Authenticate", "/ebay/auth/user/token", Method::PUT, html! {
+            h3 { "Fetch authorization code"}
+            p {
+                a href=(oauth_url) target="_blank" rel="noopener noreferrer" { "Authorization code redirect" }
+            }
+            p {
+                r#"Authenticate with your eBay "TESTUSER_*" account. After logging in, you will be redirected to a blank landing page. The landing page URL contains a "code" query parameter. Copy that code and enter it below."#
+            }
+            br;
+            h3 { "Generate user access and refresh tokens"}
             label {
                 "Authorization code"
                 input type="text" name="code";
             }
             input type="submit";
+        }))
+    }
+}
+
+fn refresh() -> Markup {
+    html! {
+        hr;
+        (form::form("Refresh user access token", "/ebay/auth/user/refresh", Method::PUT, html! {
+            button type="submit" { "Refresh" }
         }))
     }
 }
