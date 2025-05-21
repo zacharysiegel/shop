abdf10aff2a3ca8d0c0619cc2202c48fbb8df7d1
zacharysiegel <siegelzc@gmail.com>
2025-05-20 19:52:45 -0500

    build component-register in setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+set -e -u -o pipefail
+
+repo_dir=$(git rev-parse --show-toplevel)
+cd "${repo_dir}"
+
+git submodule init
+git submodule update
+
+# All setup scripts should be idempotent and callable from the repo root directory
+zsh ./identity/setup.sh
+zsh ./storefront/setup.sh
--- storefront/build.rs
+++ storefront/build.rs
@@ -1,10 +1,42 @@
+#![allow(unused)]
+
 use static_files::resource;
-use std::{env, path};
+use std::env;
+use std::fmt::{Debug, Display, Formatter};
+use std::path::Path;
 
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let out_dir = env::var("OUT_DIR")?;
-    let generated_filename = path::Path::new(&out_dir).join("public.rs");
-    resource::generate_resources_mapping("./static", None, generated_filename)?;
+    let out_dir_str: String = env::var("OUT_DIR")?;
+    let out_dir: &Path = Path::new(&out_dir_str);
+
+    static_files(out_dir)?;
 
     Ok(())
 }
+
+fn static_files(out_dir: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    let generated_filename = out_dir.join("public.rs");
+    resource::generate_resources_mapping("./static", None, generated_filename)?;
+    Ok(())
+}
+
+#[derive(Debug)]
+struct BuildError {
+    message: String,
+}
+
+impl BuildError {
+    pub fn boxed(message: &str) -> Box<BuildError> {
+        Box::new(BuildError {
+            message: message.to_string(),
+        })
+    }
+}
+
+impl Display for BuildError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Build error: {}", self.message)
+    }
+}
+
+impl std::error::Error for BuildError {}
