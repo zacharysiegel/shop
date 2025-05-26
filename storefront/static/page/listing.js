e6d397928b12ff1411d84de09ca41cbffe42f8c3
zacharysiegel <siegelzc@gmail.com>
2025-05-26 00:55:22 -0500

    refactor: x-publish-listing use sliver.js

--- storefront/static/page/listing.js
+++ storefront/static/page/listing.js
@@ -1,15 +1,18 @@
-import {register} from "../build/component-register.js";
+import {component} from "../sliver.js";
 
-register("x-publish-listing")((props, {element}) => {
-    const inner_element = document.createElement("div");
-    inner_element.innerHTML = `
-        <form data-method="PUT">
-            <button type="submit" style="margin-right: .5rem;">Publish</button>
-        </form>
-    `;
+component()
+    .properties({action: undefined})
+    .factory(({fragment, properties}) => {
+        const inner_element = document.createElement("div");
+        inner_element.innerHTML = `
+            <form data-method="PUT">
+                <button type="submit" style="margin-right: .5rem;">Publish</button>
+            </form>
+        `;
 
-    const form = inner_element.querySelector("form");
-    form.action = element.getAttribute("action");
+        const form = inner_element.querySelector("form");
+        form.action = properties.action;
 
-    element.appendChild(inner_element);
-});
+        fragment.appendChild(inner_element);
+    })
+    .define("x-publish-listing");
