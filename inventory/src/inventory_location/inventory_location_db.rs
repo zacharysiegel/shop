6ecefa5ef449151c16d8349d2df4579b99b71cd0
zacharysiegel <siegelzc@gmail.com>
2025-05-26 20:29:17 -0500

    fix: update inventory_location_db to new inventory_location model

--- inventory/src/inventory_location/inventory_location_db.rs
+++ inventory/src/inventory_location/inventory_location_db.rs
@@ -6,14 +6,19 @@ pub async fn create_inventory_location(
     pgpool: &PgPool,
     inventory_location: InventoryLocationEntity,
 ) -> Result<PgQueryResult, Error> {
-    query!(
-		"\
-		insert into shop.public.inventory_location (id, display_name, internal_name) \
-		values ($1, $2, $3)\
+    query!("
+		insert into shop.public.inventory_location (id, display_name, internal_name, time_zone_id, street_address, municipality, district, postal_area, country) 
+		values ($1, $2, $3, $4, $5, $6, $7, $8, $9)
 	",
 		inventory_location.id,
 		inventory_location.display_name,
-		inventory_location.internal_name
+		inventory_location.internal_name,
+        inventory_location.time_zone_id,
+        inventory_location.street_address,
+        inventory_location.municipality,
+        inventory_location.district,
+        inventory_location.postal_area,
+        inventory_location.country,
 	)
         .execute(pgpool)
         .await
@@ -22,13 +27,10 @@ pub async fn create_inventory_location(
 pub async fn get_all_inventory_locations(
     pgpool: &PgPool,
 ) -> Result<Vec<InventoryLocationEntity>, Error> {
-    query_as!(
-		InventoryLocationEntity,
-		"\
-    	select id, display_name, internal_name \
-		from shop.public.inventory_location \
-	"
-	)
+    query_as!(InventoryLocationEntity, "
+    	select id, display_name, internal_name, time_zone_id, street_address, municipality, district, postal_area, country 
+		from shop.public.inventory_location 
+	")
         .fetch_all(pgpool)
         .await
 }
