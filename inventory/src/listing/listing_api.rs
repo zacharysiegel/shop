0f2a6b88a7c32720d47ae8bf4de6864ec7a89505
zacharysiegel <siegelzc@gmail.com>
2025-05-29 23:15:27 -0500

    remove publish_listing endpoint

--- inventory/src/listing/listing_api.rs
+++ inventory/src/listing/listing_api.rs
@@ -1,6 +1,6 @@
-use crate::listing::{listing_db, Listing, ListingEntity, ListingSerial};
+use crate::listing::{listing_db, Listing, ListingSerial};
 use crate::object::JsonHttpResponse;
-use crate::{marketplace, unwrap_option_else_404, unwrap_result_else_400, unwrap_result_else_500, ShopEntity, ShopModel, ShopSerial};
+use crate::{unwrap_option_else_404, unwrap_result_else_400, unwrap_result_else_500, ShopEntity, ShopModel, ShopSerial};
 use actix_web::web::ServiceConfig;
 use actix_web::{web, HttpResponse, Responder};
 use sqlx::postgres::PgQueryResult;
@@ -13,7 +13,6 @@ pub fn configurer(config: &mut ServiceConfig) {
             .route("", web::post().to(create_listing))
             .route("/{listing_id}", web::get().to(get_listing))
             .route("/{listing_id}", web::put().to(update_listing))
-            .route("/{listing_id}/publish", web::put().to(publish_listing))
     );
 }
 
@@ -55,20 +54,4 @@ async fn update_listing(
     let query_result: PgQueryResult =
         unwrap_result_else_500!(listing_db::update_listing(&pgpool, &listing.to_entity()).await);
     HttpResponse::Ok().body(query_result.rows_affected().to_string())
-}
-
-async fn publish_listing(
-    pgpool: web::Data<PgPool>,
-    listing_id: web::Path<String>,
-) -> impl Responder {
-    let listing_id: Uuid = unwrap_result_else_400!(Uuid::try_parse(&listing_id.into_inner()));
-
-    let listing_entity: Option<ListingEntity> = unwrap_result_else_500!(listing_db::get_listing(&pgpool, &listing_id).await);
-    let listing_entity: ListingEntity = unwrap_option_else_404!(listing_entity);
-    let listing: Listing = unwrap_result_else_500!(listing_entity.try_to_model());
-
-    unwrap_result_else_500!(marketplace::ebay::ebay_action::publish(&pgpool, &listing).await);
-    unwrap_result_else_500!(listing_db::publish_listing(&pgpool, &listing_id).await);
-
-    HttpResponse::Ok().finish()
 }
