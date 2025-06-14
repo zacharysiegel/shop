ed46de0046275a1a537151f26be692213812b08f
Zachary Siegel <siegelzc@gmail.com>
2025-06-13 19:25:49 -0500

    V6__marketplace_values.sql

---
+++ schema/migrations/V6__marketplace_values.sql
@@ -0,0 +1,3 @@
+insert into shop.public.marketplace (id, display_name, internal_name, uri)
+values (gen_random_uuid(), 'eBay', 'ebay', null),
+       (gen_random_uuid(), 'Mercari', 'mercari', null);
