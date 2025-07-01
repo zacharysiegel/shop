45bdea9adebab72dab3934a9320a0dea6e81aa62
zacharysiegel <siegelzc@gmail.com>
2025-06-30 21:29:08 -0500

    add original_file_name item_image column

--- schema/migrations/V7__item_image_update.sql
+++ schema/migrations/V7__item_image_update.sql
@@ -1,3 +1,4 @@
 alter table if exists shop.public.item_image
-    drop column if exists uri
+    drop column if exists uri,
+    add column if not exists original_file_name text not null default 'invalid'
 ;
