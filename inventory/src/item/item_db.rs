33c524da27923faee78b70beef8b3912cf71366d
zacharysiegel <siegelzc@gmail.com>
2025-07-01 23:17:24 -0500

    get_all_item_images

--- inventory/src/item/item_db.rs
+++ inventory/src/item/item_db.rs
@@ -1,4 +1,5 @@
 use super::*;
+use crate::item_image::ItemImageEntity;
 use crate::label::LabelEntity;
 use crate::listing::ListingEntity;
 use sqlx::postgres::PgQueryResult;
@@ -18,38 +19,36 @@ pub async fn get_item(pgpool: &PgPool, item_id: &Uuid) -> Result<Option<ItemEnti
 }
 
 pub async fn create_item(pgpool: &PgPool, item: &ItemEntity) -> Result<PgQueryResult, Error> {
-    query!("\
-		insert into shop.public.item (id, product_id, inventory_location_id, condition, status, price_cents, priority, note, acquisition_datetime, acquisition_price_cents, acquisition_location, created, updated)\
-		values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)\
-		",
-	item.id,
-	item.product_id,
-	item.inventory_location_id,
-	item.condition,
-	item.status,
-	item.price_cents,
-	item.priority,
-	item.note,
-	item.acquisition_datetime,
-	item.acquisition_price_cents,
-	item.acquisition_location,
-	item.created,
-	item.updated,
+    query!("
+		insert into shop.public.item (id, product_id, inventory_location_id, condition, status, price_cents, priority, note, acquisition_datetime, acquisition_price_cents, acquisition_location, created, updated)
+		values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+	",
+		item.id,
+		item.product_id,
+		item.inventory_location_id,
+		item.condition,
+		item.status,
+		item.price_cents,
+		item.priority,
+		item.note,
+		item.acquisition_datetime,
+		item.acquisition_price_cents,
+		item.acquisition_location,
+		item.created,
+		item.updated,
 	)
         .execute(pgpool)
         .await
 }
 
 pub async fn get_all_item_labels(pgpool: &PgPool, item_id: &Uuid) -> Result<Vec<LabelEntity>, Error> {
-    query_as!(
-		LabelEntity,
-		"
+    query_as!(LabelEntity, "
         select id, display_name, internal_name
 		from shop.public.label
         inner join shop.public.item_label_association on label.id = item_label_association.label_id
         where item_label_association.item_id = $1
     ",
-		item_id
+		item_id,
 	)
         .fetch_all(pgpool)
         .await
@@ -60,13 +59,12 @@ pub async fn create_item_label_association(
     item_id: &Uuid,
     label_id: &Uuid,
 ) -> Result<PgQueryResult, Error> {
-    query!(
-		"\
-		insert into shop.public.item_label_association (item_id, label_id) \
-		values ($1, $2) \
+    query!("
+		insert into shop.public.item_label_association (item_id, label_id)
+		values ($1, $2)
 	",
 		item_id,
-		label_id
+		label_id,
 	)
         .execute(pgpool)
         .await
@@ -77,13 +75,12 @@ pub async fn delete_item_label_association(
     item_id: &Uuid,
     label_id: &Uuid,
 ) -> Result<PgQueryResult, Error> {
-    query!(
-		"\
-		delete from shop.public.item_label_association \
-		where item_id = $1 and label_id = $2 \
+    query!("
+		delete from shop.public.item_label_association
+		where item_id = $1 and label_id = $2
 	",
 		item_id,
-		label_id
+		label_id,
 	)
         .execute(pgpool)
         .await
@@ -93,12 +90,25 @@ pub async fn get_all_item_listings(
     pgpool: &PgPool,
     item_id: &Uuid,
 ) -> Result<Vec<ListingEntity>, Error> {
-    query_as!(
-		ListingEntity,
-		"\
-		select * \
-		from shop.public.listing \
-		where item_id = $1 \
+    query_as!(ListingEntity, "
+		select id, item_id, marketplace_id, status, created, updated
+		from shop.public.listing
+		where item_id = $1
+	",
+		item_id,
+	)
+        .fetch_all(pgpool)
+        .await
+}
+
+pub async fn get_all_item_images(
+    pgpool: &PgPool,
+    item_id: &Uuid,
+) -> Result<Vec<ItemImageEntity>, Error> {
+    query_as!(ItemImageEntity, "\
+		select id, item_id, alt_text, priority, original_file_name
+		from shop.public.item_image
+		where item_id = $1
 	",
 		item_id,
 	)
