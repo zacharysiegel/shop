20dc33e24f5ef7a7819a5de2aaa1549de2b86023
zacharysiegel <siegelzc@gmail.com>
2025-07-06 08:10:44 -0500

    get postgres password from crypt during runtime

--- inventory/src/db.rs
+++ inventory/src/db.rs
@@ -1,14 +1,19 @@
+use crate::decrypt::master_decrypt;
+use crate::error::ShopError;
 use sqlx::postgres::PgPoolOptions;
 use sqlx::{Error, Pool, Postgres};
 
-pub async fn sqlx_connect() -> Result<Pool<Postgres>, std::io::Error> {
+pub async fn sqlx_connect() -> Result<Pool<Postgres>, ShopError> {
+    let password: Vec<u8> = master_decrypt("postgres__user.shop.password")?;
+    let password: String = String::from_utf8(password)
+        .map_err(|e| ShopError::from_error_default(Box::new(e)))?;
     let pool_result: Result<Pool<Postgres>, Error> = PgPoolOptions::new()
         .max_connections(16)
-        .connect("postgres://user:password@localhost:5432/shop")
+        .connect(&format!("postgres://user:{}@localhost:5432/shop", password))
         .await;
 
     match pool_result {
         Ok(pool) => Ok(pool),
-        Err(error) => Err(std::io::Error::new(std::io::ErrorKind::Other, error)),
+        Err(error) => Err(ShopError::from(error)),
     }
 }
--- inventory/src/main.rs
+++ inventory/src/main.rs
@@ -1,15 +1,17 @@
 use inventory::{ebay, environment, server};
 use sqlx::{Pool, Postgres};
+use std::io;
 
 #[actix_web::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> Result<(), io::Error> {
     environment::load_env()?;
     environment::init_logger()
-        .map_err(|err| std::io::Error::new(std::io::ErrorKind::Other, err))?;
+        .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
 
     log::info!("Runtime environment: {:?}", environment::RuntimeEnvironment::default());
 
-    let pgpool: Pool<Postgres> = inventory::db::sqlx_connect().await?;
+    let pgpool: Pool<Postgres> = inventory::db::sqlx_connect().await
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
 
     ebay::ebay_action::init(&pgpool).await;
 
