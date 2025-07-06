539aedf20240eb86152470e3022111bb4ff5ca20
zacharysiegel <siegelzc@gmail.com>
2025-07-06 07:44:35 -0500

    schema: fetch password from crypt using master key (on environment)

--- schema/src/main.rs
+++ schema/src/main.rs
@@ -1,17 +1,20 @@
-use std::time::Duration;
-
+use base64::Engine;
 use postgres::{Client, Config, NoTls};
+use std::env;
+use std::error::Error;
+use std::time::Duration;
+use crypt::{cryptography, BASE64};
 
 refinery::embed_migrations!("migrations");
 
-fn main() -> Result<(), postgres::Error> {
+fn main() -> Result<(), Box<dyn Error>> {
     env_logger::builder()
         .filter_level(log::LevelFilter::Info)
         .init();
 
     let mut postgres_config = Config::new();
     postgres_config.user("user");
-    postgres_config.password("password");
+    postgres_config.password(get_shop_password()?);
     postgres_config.dbname("shop");
     postgres_config.host("localhost");
     postgres_config.port(5432);
@@ -26,7 +29,7 @@ fn main() -> Result<(), postgres::Error> {
         log::info!("Migration applied: {}", migration)
     }
 
-    Result::Ok(())
+    Ok(())
 }
 
 fn check_connection(client: &mut Client) -> Result<(), postgres::Error> {
@@ -40,4 +43,19 @@ fn check_connection(client: &mut Client) -> Result<(), postgres::Error> {
         return Err(e);
     }
     Ok(())
+}
+
+fn get_shop_password() -> Result<String, Box<dyn Error>> {
+    let master_secret: Vec<u8> = get_master_secret()?;
+    let secret: Vec<u8> = cryptography::decrypt(&master_secret, "postgres__user.shop.password")?;
+    let secret: String = String::from_utf8(secret)?;
+    Ok(secret)
+}
+
+/// Decrypt a secret managed by the `crypt` application.
+fn get_master_secret() -> Result<Vec<u8>, Box<dyn Error>> {
+    let variable_name = "MASTER_SECRET";
+    let master_secret: String = env::var(variable_name)?;
+    let master_secret: Vec<u8> = BASE64.decode(master_secret)?;
+    Ok(master_secret)
 }
