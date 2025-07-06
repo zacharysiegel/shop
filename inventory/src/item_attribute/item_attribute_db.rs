10d33c62abab3ff3bbc59b8888f1dc73a7da52f8
zacharysiegel <siegelzc@gmail.com>
2025-07-06 11:12:10 -0500

    same for item_attribute_db

--- inventory/src/item_attribute/item_attribute_db.rs
+++ inventory/src/item_attribute/item_attribute_db.rs
@@ -2,75 +2,74 @@ use crate::item_attribute::ItemAttributeEntity;
 use sqlx::postgres::PgQueryResult;
 use sqlx::{query, query_as, PgPool};
 use uuid::Uuid;
+use crate::error::ShopError;
 
 pub async fn get_item_attribute(
     pgpool: &PgPool,
     item_id: &Uuid,
     key: &str,
-) -> Result<Option<ItemAttributeEntity>, sqlx::Error> {
-    query_as!(
-		ItemAttributeEntity,
-		"\
-        select item_id, key, value, visible, priority \
-        from shop.public.item_attribute \
-        where item_id = $1 and key = $2 \
+) -> Result<Option<ItemAttributeEntity>, ShopError> {
+    query_as!(ItemAttributeEntity, "
+        select item_id, key, value, visible, priority
+        from shop.public.item_attribute
+        where item_id = $1 and key = $2
     ",
 		item_id,
-		key
+		key,
 	)
         .fetch_optional(pgpool)
         .await
+		.map_err(|e| ShopError::from(e))
 }
 
 pub async fn get_all_item_attributes(
     pgpool: &PgPool,
     item_id: &Uuid,
-) -> Result<Vec<ItemAttributeEntity>, sqlx::Error> {
-    query_as!(
-		ItemAttributeEntity,
-		"\
-        select item_id, key, value, visible, priority \
-        from shop.public.item_attribute \
-        where item_id = $1 \
+) -> Result<Vec<ItemAttributeEntity>, ShopError> {
+    query_as!(ItemAttributeEntity, "
+        select item_id, key, value, visible, priority
+        from shop.public.item_attribute
+        where item_id = $1
     ",
-		item_id
+		item_id,
 	)
         .fetch_all(pgpool)
         .await
+		.map_err(|e| ShopError::from(e))
 }
 
 pub async fn create_item_attribute(
     pgpool: &PgPool,
     item_attribute: &ItemAttributeEntity,
-) -> Result<PgQueryResult, sqlx::Error> {
-    query!(
-		"\
-        insert into shop.public.item_attribute (item_id, key, value, visible, priority) \
-        values ($1, $2, $3, $4, $5) \
+) -> Result<PgQueryResult, ShopError> {
+    query!("
+        insert into shop.public.item_attribute (item_id, key, value, visible, priority)
+        values ($1, $2, $3, $4, $5)
     ",
 		item_attribute.item_id,
 		item_attribute.key,
 		item_attribute.value,
 		item_attribute.visible,
-		item_attribute.priority
+		item_attribute.priority,
 	)
         .execute(pgpool)
         .await
+		.map_err(|e| ShopError::from(e))
 }
 
 pub async fn delete_item_attribute(
     pgpool: &PgPool,
     item_id: &Uuid,
     key: &str,
-) -> Result<PgQueryResult, sqlx::Error> {
-    query!(
-		"\
-        delete from shop.public.item_attribute \
-        where item_id = $1 and key = $2 \
+) -> Result<PgQueryResult, ShopError> {
+    query!("
+        delete from shop.public.item_attribute
+        where item_id = $1 and key = $2
     ",
 		item_id,
-		key
+		key,
 	)
         .execute(pgpool)
         .await
+		.map_err(|e| ShopError::from(e))
 }
