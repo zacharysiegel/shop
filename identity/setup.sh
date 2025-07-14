ab8fd2fb3f4ac0230ae34171e3a0ab66bb528ff3
zacharysiegel <siegelzc@gmail.com>
2025-07-14 17:38:32 -0500

    configuration.<env>.yaml

--- identity/setup.sh
+++ identity/setup.sh
@@ -1,15 +1,80 @@
 #!/bin/zsh
 
+function prepare_env_file {
+	if test -e .env; then
+		echo "Environment file identity/.env already exists; Copying to identity/.env.bak;"
+		cp .env .env.bak
+		rm .env
+	fi
+}
+
+function generate_secrets {
+	properties="$(yq < ./secret/index.yaml '.[]')"
+
+	while IFS= read -r property; do
+		echo "Processing $property"
+
+		environment_variable=$(
+			sed <<< "$property" \
+				-E -e 's/\./_/g; y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/; s/^/AUTHELIA_/; s/$/_FILE/'
+		)
+		file_name="${property}.txt"
+
+		echo "${environment_variable}=/secret/${file_name}" >> .env
+
+		secret_name="authelia__${property}"
+		secret=$(
+			cargo run -p crypt -- decrypt --utf8 --key "$master_key" "$secret_name"
+		)
+		echo "$secret" > "./secret/${file_name}"
+	done <<< "$properties"
+}
+
+function generate_configurations {
+	configuration_dir="${repo_path}/identity/config/"
+	configuration_path_template="${configuration_dir}/configuration.template.yaml"
+
+	session_cookies_0_domain="session.cookies.0.domain"
+	session_cookies_0_authelia_url="session.cookies.0.authelia_url"
+	session_cookies_0_default_redirection_url="session.cookies.0.default_redirection_url"
+
+	echo "Generating environment-specific configuration files from ${configuration_path_template}"
+
+	# prod
+	sed > "${configuration_dir}/configuration.prod.yaml" \
+		-E \
+		-e "s/${session_cookies_0_domain}/shop.zach.ro/g" \
+		-e "s/${session_cookies_0_authelia_url}/https:\/\/shop.zach.ro\/auth/g" \
+		-e "s/${session_cookies_0_default_redirection_url}/https:\/\/shop.zach.ro\/admin/g" \
+		"${configuration_path_template}"
+
+	# stage
+	sed > "${configuration_dir}/configuration.stage.yaml" \
+		-E \
+		-e "s/${session_cookies_0_domain}/shop-stage.zach.ro/g" \
+		-e "s/${session_cookies_0_authelia_url}/https:\/\/shop-stage.zach.ro\/auth/g" \
+		-e "s/${session_cookies_0_default_redirection_url}/https:\/\/shop-stage.zach.ro\/admin/g" \
+		"${configuration_path_template}"
+
+	# local
+	sed > "${configuration_dir}/configuration.local.yaml" \
+		-E \
+		-e "s/${session_cookies_0_domain}/127.0.0.1/g" \
+		-e "s/${session_cookies_0_authelia_url}/https:\/\/127.0.0.1:1443\/auth/g" \
+		-e "s/${session_cookies_0_default_redirection_url}/https:\/\/127.0.0.1:1443\/admin/g" \
+		"${configuration_path_template}"
+}
+
 set -euo pipefail
 
 master_key="${1?"Argument 1 required: master_key"}"
 
-if ! which yq 1>/dev/null 2>&1; then
+if ! which yq 1> /dev/null 2>&1; then
 	echo 'The `yq` program is required'
 	exit 1
 fi
 
-if ! which cargo 1>/dev/null 2>&1; then
+if ! which cargo 1> /dev/null 2>&1; then
 	echo 'The `cargo` program is required'
 	exit 1
 fi
@@ -17,28 +82,6 @@ fi
 repo_path=$(git rev-parse --show-toplevel)
 cd "${repo_path}/identity" || exit
 
-properties="$(< ./secret/index.yaml yq '.[]')"
-
-if test -e .env; then
-	echo "Environment file identity/.env already exists; Copying to identity/.env.bak;"
-	cp .env .env.bak
-	rm .env
-fi
-
-while IFS= read -r property; do
-    echo "Processing $property"
-
-    environment_variable=$(\
-        <<< "$property" \
-        sed -E -e 's/\./_/g; y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/; s/^/AUTHELIA_/; s/$/_FILE/' \
-    )
-    file_name="${property}.txt"
-
-    echo "${environment_variable}=/secret/${file_name}" >> .env
-
-	secret_name="authelia__${property}"
-	secret=$(\
-		cargo run -p crypt -- decrypt --utf8 --key "$master_key" "$secret_name" \
-	)
-	echo "$secret" > "./secret/${file_name}"
-done <<< "$properties"
+prepare_env_file
+generate_secrets
+generate_configurations
